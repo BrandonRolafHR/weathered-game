ca37f7b4bd1819047c6d4ff37842d5e12c4b1872
brand <1125796@hr.nl>
2026-06-15 11:24:29 +0200

    excalibur now including neccessary files

--- src/js/game.js
+++ src/js/game.js
@@ -1,38 +1,10 @@
 import '../css/style.css'
-import { Actor, Engine, Vector, DisplayMode, randomInRange } from "excalibur"
+import { Actor, Engine, Vector, DisplayMode } from "excalibur"
 import { Resources, ResourceLoader } from './resources.js'
-import { Background } from './class/background.js'
-import { Cowboy } from './class/cowboy.js'
-import { Cactus } from './class/cactus.js'
-import { Bullet } from './class/bullet.js'
+
 
 export class Game extends Engine {
 
-    constructor() {
-        super({ 
-            width: 1280,
-            height: 720,
-            maxFps: 60,
-            displayMode: DisplayMode.FitScreen
-         })
-        this.start(ResourceLoader).then(() => this.startGame());
-    }
-
-    startGame() {
-        const loadBackground = new Background();
-        this.add(loadBackground);
-
-        const loadCowboy = new Cowboy(); 
-        this.add(loadCowboy);
-
-        const loadBullet = new Bullet()
-        this.add(loadBullet)
-    }
-
-    spawnCactus() {
-        const loadCactus = new Cactus();
-        this.add(loadCactus);
-    }
 }
 
 new Game()
--- src/js/resources.js
+++ src/js/resources.js
@@ -2,13 +2,7 @@ import { ImageSource, Sound, Resource, Loader, ImageWrapping } from 'excalibur'
 
 // voeg hier jouw eigen resources toe
 const Resources = {
-    Cowboy: new ImageSource('images/cowman1.png'),
-    Cowboy2: new ImageSource('images/cowman2.png'),
-    Cactus: new ImageSource('images/cactus.png'),
-    Bullet: new ImageSource('images/bullet.png'),
-    Bandit1: new ImageSource('images/bandit1.png'),
-    Bandit2: new ImageSource('images/bandit2.png'),
-    Background: new ImageSource('images/desert-background.png', { wrapping: ImageWrapping.Repeat}),
+
 }
 
 const ResourceLoader = new Loader()
