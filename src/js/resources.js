980b0d1b62315a0cd098c4a126139baa47b58ddf
Joep Nijbacker <joepnij@icloud.com>
2026-06-17 11:15:46 +0200

    background + platform

--- src/js/resources.js
+++ src/js/resources.js
@@ -2,6 +2,8 @@ import { ImageSource, Sound, Resource, Loader, ImageWrapping } from 'excalibur'
 
 const Resources = {
     PlayerOne: new ImageSource('images/bassie.png'),
+    Background: new ImageSource('images/standard-denver.png'),
+    Ground: new ImageSource('images/standard-platform.png'),
 }
 
 const ResourceLoader = new Loader()
