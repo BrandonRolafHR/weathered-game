1517a6f6ae89871b9239724e62113ddcbf140cf6
BBaxe <63966085+BBaxe@users.noreply.github.com>
2026-06-17 15:28:21 +0200

    Branch + damage op player

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,9 +1,11 @@
 import { ImageSource, Sound, Resource, Loader, ImageWrapping } from 'excalibur'
+import { Branch } from './class/branch'
 
 const Resources = {
     PlayerOne: new ImageSource('images/bassie.png'),
     Background: new ImageSource('images/standard-denver.png'),
     Ground: new ImageSource('images/standard-platform.png'),
+    Branch: new ImageSource('images/branch.png')
 }
 
 const ResourceLoader = new Loader()
