db144efc0be40049560836b80938a539b49ffe94
brand <1125796@hr.nl>
2026-06-18 13:45:05 +0200

    hopefully is the lightning scene working now

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,11 +1,17 @@
 import { ImageSource, Sound, Resource, Loader, ImageWrapping } from 'excalibur'
 import { Branch } from './class/branch'
+import { ThunderBackground } from './class/thunderbackground'
+import { ThunderPlatform } from './class/thunderplatform'
 
 const Resources = {
     PlayerOne: new ImageSource('images/bassie.png'),
     Background: new ImageSource('images/standard-denver.png'),
     Ground: new ImageSource('images/standard-platform.png'),
-    Branch: new ImageSource('images/branch.png')
+    Branch: new ImageSource('images/branch.png'),
+
+    ThunderPlatform: new ImageSource('images/thunder-background.png'),
+    ThunderBackground: new ImageSource('images/thunder-platform.png'),
+    Lightning: new ImageSource('images/lightning.png'),
 }
 
 const ResourceLoader = new Loader()
